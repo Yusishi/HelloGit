0d9f8bf9072a02bdca86bcccbf3dc108db98b686
Yusishi <geweijian2013@163.com>
2020-06-07 14:30:03 +0800

    123

--- HelloGit/HelloGit/src/main/java/com/test/HelloGit.java
+++ HelloGit/HelloGit/src/main/java/com/test/HelloGit.java
@@ -2,6 +2,16 @@ package com.test;
 
 public class HelloGit {
     public static void main(String[] args) {
+
+
+
+        new Thread(() -> {
+            for (int i = 1; i <=10 ; i++)
+            {
+                System.out.println(Thread.currentThread().getName());
+            }
+        },"input thread name").start();
+
         System.out.println("HelloGit");
     }
 }
